52a9f15c8f5d2e919f95020bafac7408e6d6ad63
Rishi Ghan <rishi.ghan@gmail.com>
2012-05-23 16:29:46 -0400

    simple lexicon, that translates english to swedish

---
+++ swedish.js
@@ -0,0 +1,15 @@
+var lexicon = {"merry":"god", "christmas":"jul", "and":"och", "happy":"gott", "new":"nytt", "year":"år"},
+
+    english ="merry christmas and a happy new year",
+    splitted = english.split(" "),
+    temp = [];
+
+for(t in lexicon)
+{
+	if(splitted.indexOf(t,0)!== -1)
+	{
+	  temp.push(lexicon[t]);
+	}
+}
+
+alert(temp);
