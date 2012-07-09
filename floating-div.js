74039f10e8590f0379ee28c13d18c60670e988fb
Rishi Ghan <rishi.ghan@gmail.com>
2012-07-09 18:56:04 -0400

    changes

--- floating-div.js
+++ floating-div.js
@@ -3,9 +3,18 @@
 
 function floatDiv(element)
 {
-	var foo = document.getElementById(element);
-	//console.log(foo.id);
-	console.log(foo.style.left);
+	
+ var foo = document.getElementById(element),
+
+	 tall = foo.offsetHeight,
+	 wide = foo.offsetWidth,
+	 docheight = document.body.clientHeight,
+	 scrollheight = document.body.scrollHeight;
+	
+	window.onscroll = function moveThis(){
+	 console.log('moved');
+	}
+		
 }
 
 floatDiv('sidebar');
