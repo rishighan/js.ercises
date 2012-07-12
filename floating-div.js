1a81312cf4c25f9d211018e65c753bc662bb3531
Rishi Ghan <rishi.ghan@gmail.com>
2012-07-12 11:36:17 -0400

    canvas with 2d drawing

--- floating-div.js
+++ floating-div.js
@@ -1,20 +1,35 @@
 // Rishi Ghan
 // Ninth Pixie
 
+// 2d drawing in Javascript
+function drawGrid(canvasID){
+	
+	var canvas = document.getElementById(canvasID),
+	    context = canvas.getContext('2d');
+	
+		context.fillStyle="#000";
+		context.beginPath();
+		context.moveTo(0,0);
+		context.lineTo(0,900);
+		
+		
+}
+
 function floatDiv(element)
 {
 	
  var foo = document.getElementById(element),
-
-	 tall = foo.offsetHeight,
-	 wide = foo.offsetWidth,
-	 docheight = document.body.clientHeight,
-	 scrollheight = document.body.scrollHeight;
-	
-	window.onscroll = function moveThis(){
-	 console.log('moved');
+     scrolltop = document.body.scrollTop;  
+	 
+	 window.onscroll = function(){
+		var windowscrollpos = window.pageYOffset;
+		if(windowscrollpos===0) foo.style.top = '0px';
+		else{
+		foo.style.top = windowscrollpos+"px";
 	}
-		
+}
+
 }
 
 floatDiv('sidebar');
+drawGrid('gridder');
