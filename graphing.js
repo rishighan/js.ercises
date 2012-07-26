acd9b3556f829770ed62d6c446f2a6f63a828bb8
Rishi Ghan <rishi.ghan@gmail.com>
2012-07-26 16:33:39 -0400

    JSsssing.

--- graphing.js
+++ graphing.js
@@ -1,33 +1,51 @@
 // rishi ghan
-// trying to create a graphing class that is general purpose
+// trying to create a general purpose graphing class 
 // july 26, 2012
 
 function Graphr (xdata, ydata, divid){
-	var totaldatapoints = 0,
+	var sum = 0,
 		percentages =[],
-		angles =[];
-	this.kind = function() {console.log("pie")};
+		angles =[],
+		canvas = document.getElementById(divid);
+		
+	this.kind = function() {console.log("pie")},
+	
+	// sums up the elements in an array
+	this.sum = function(arr) {
+		for(var i=0; i<arr.length; i++)
+		{
+			sum+= arr[i];
+		}
+		return sum;
+	},
+	
+	// ??
 	this.xdatapoints =  function(){
-		 totaldatapoints = xdata.length;
+		 var totaldatapoints = this.sum(xdata);
 		 
 		 // loop through data points array and calculate percentages.
 		for (var i=0; i < xdata.length; i++) {
-			percentages[i] = Math.ceil((xdata[i]/totaldatapoints) *100);
-			console.log(percentages[i]);
+			percentages[i] = Math.ceil((xdata[i]/totaldatapoints) *100); // percentages
+			angles[i] = Math.ceil((xdata[i]/totaldatapoints) *360)  //angles
+			//console.log(angles[i]);
 		};
 		
+		// find the fucking canvas element
+		context = canvas.getContext("2d");
 		
-	};
+	    //co-ordinates (where should they be initialized)
+		
+	},
 	this.ydatapoints =  function(){
 		console.log(ydata);
 	}
-}
+} //end the class definition
 
 
-//
-var arr1= [1,2,3],
+// testing
+var arr1= [1,2,1],
 	arr2 = [4,5,6];
-var g = new Graphr(arr1,arr2);
+var g = new Graphr(arr1,arr2,"grid");
 		g.kind(); // this is how you call a method.
 		g.ydatapoints(); // printing out the contents of an array
 		g.xdatapoints();
