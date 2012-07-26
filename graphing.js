c123177da9bc5cbd584212272402bc5cfa8a914c
Rishi Ghan <rishi.ghan@gmail.com>
2012-07-26 15:08:40 -0400

    adding javascript

--- graphing.js
+++ graphing.js
@@ -2,19 +2,32 @@
 // trying to create a graphing class that is general purpose
 // july 26, 2012
 
-function Graphr (xdata,ydata){
-	
-	this.type = "pie";
+function Graphr (xdata, ydata, divid){
+	var totaldatapoints = 0,
+		percentages =[],
+		angles =[];
+	this.kind = function() {console.log("pie")};
 	this.xdatapoints =  function(){
-		alert(xdata);
+		 totaldatapoints = xdata.length;
+		 
+		 // loop through data points array and calculate percentages.
+		for (var i=0; i < xdata.length; i++) {
+			percentages[i] = Math.ceil((xdata[i]/totaldatapoints) *100);
+			console.log(percentages[i]);
+		};
+		
+		
 	};
 	this.ydatapoints =  function(){
 		console.log(ydata);
 	}
 }
 
+
+//
 var arr1= [1,2,3],
 	arr2 = [4,5,6];
 var g = new Graphr(arr1,arr2);
-		g.xdatapoints;
-		g.ydatapoints;
+		g.kind(); // this is how you call a method.
+		g.ydatapoints(); // printing out the contents of an array
+		g.xdatapoints();
