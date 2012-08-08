016bf4c7b3d8cf814d0c458742735db7e277257b
Rishi Ghan <rishi.ghan@gmail.com>
2012-08-08 18:34:09 -0400

    drawing table and tr, td

--- domops.js
+++ domops.js
@@ -10,25 +10,45 @@ var Calendar = function(){
 		
 		var args = arguments.length,
 		    month = month || 0,
-		    year = year || 2012; //default [0,1,2,3,4,5,6]
+		    year = year || 2012, //default [0,1,2,3,4,5,6]
+		    weekdays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
 		if(typeof(month)==='number' && typeof(year) === 'number')
 		{
 			// this just finds out if a month has 
 			
-			var days = new Date(year, month, 0).getDate();
-				dayofweek = 0, // thats a sunday
-				h = 0;
-	
-				console.log(" Year: "+year+ " Month: "+ m + " Total Days: "+days);
+			var days = new Date(year, month, 0).getDate(),
+				h = [];
+				//console.log(" Year: "+year+ " Month: "+ month + " Total Days: "+days);
 				
-			// loop and print number of days
-			for (var q=1; q <= days; q++) {
+			// Zeller's algorithm http://en.wikipedia.org/wiki/Zeller's_congruence
+			for (var q = 1; q <= days; q++) {
 				if (month < 3) { month += 12; year -= 1; }
-				// Zeller's algorithm
-				var h = (q + parseInt(((month + 1) * 26) / 10) + year + parseInt(year / 4) + 6 * parseInt(year / 100) + parseInt(year / 400) - 1) % 7;
-				console.log(h+"->"+ q);
+				
+				h.push((q + parseInt(((month + 1) * 26) / 10) + year + parseInt(year / 4) + 6 * parseInt(year / 100) + parseInt(year / 400) - 1) % 7);
+				//console.log(h+"->"+ q);
+
 			};
+			return h;
 		}
+		
+		//print out the weekdays
+		var target = document.getElementById('parent'),
+			tab = document.createElement('table'),
+		    weekdayrow = document.createElement('tr'),
+			weekdayname = [];
+			
+		    
+		    target.appendChild(tab); // add table
+			tab.appendChild(weekdayrow); // add weekday row
+			
+		for (var i=0; i < weekdays.length; i++) {
+			
+			var weekdaycolumn = document.createElement('td') // create a row
+			weekdayname[i] = document.createTextNode(weekdays[i]); 
+			weekdayrow.appendChild(weekdaycolumn); // adding rows
+			weekdaycolumn.appendChild(weekdayname[i]);
+			
+		};
 	}
 	
 
@@ -36,4 +56,4 @@ var Calendar = function(){
 
 
 var cal = new Calendar;
-cal.buildMonth(12, 2012);
+cal.buildMonth(8, 2012);
