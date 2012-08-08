919aa8b77045d45b7cb2ed682b3f4f7f1190b0b7
Rishi Ghan <rishi.ghan@gmail.com>
2012-08-08 16:36:15 -0400

    added Zeller's algorithm for figuring out day of the week the date falls on.

--- domops.js
+++ domops.js
@@ -3,17 +3,37 @@
 
 // August 6, 2012
 
-var Creator =  function(elem){
+var Calendar = function(){
 	
-	this.multiply = function(){
-	var ele = document.getElementById(elem),
-	      p = document.createElement("span"),
-	      q = document.createTextNode("text");
-    ele.appendChild(p); p.appendChild(q);
+	// build a month with weekdays, weekends etc.
+	this.buildMonth =function(month,year){
+		
+		var args = arguments.length,
+		    month = month || 0,
+		    year = year || 2012; //default [0,1,2,3,4,5,6]
+		if(typeof(month)==='number' && typeof(year) === 'number')
+		{
+			// this just finds out if a month has 
+			
+			var days = new Date(year, month, 0).getDate();
+				dayofweek = 0, // thats a sunday
+				h = 0;
 	
+				console.log(" Year: "+year+ " Month: "+ m + " Total Days: "+days);
+				
+			// loop and print number of days
+			for (var q=1; q <= days; q++) {
+				if (month < 3) { month += 12; year -= 1; }
+				// Zeller's algorithm
+				var h = (q + parseInt(((month + 1) * 26) / 10) + year + parseInt(year / 4) + 6 * parseInt(year / 100) + parseInt(year / 400) - 1) % 7;
+				console.log(h+"->"+ q);
+			};
+		}
 	}
+	
+
 }
 
 
-var foo = new Creator('parent');
-foo.multiply();
+var cal = new Calendar;
+cal.buildMonth(12, 2012);
