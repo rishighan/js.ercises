f3d10eafa8cf5f5ac5e4ead7395374a645a2e6e4
Rishi Ghan <rishi.ghan@gmail.com>
2012-08-15 15:23:49 -0400

    Working calendar! TODO: Add month name and year to denote what month you are looking at.

--- calendar.js
+++ calendar.js
@@ -10,12 +10,11 @@ var Calendar = function(){
 		
 		var args = arguments.length,
 		    month = month || 0,
-		    year = year || 2012, //default [0,1,2,3,4,5,6]
+		    year = year || new Date().getFullYear(), 
 		    weekdays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
 		if(typeof(month)==='number' && typeof(year) === 'number')
 		{
-			// this just finds out if a month has 
-			
+			// this just finds out how many days a month has 
 			var days = new Date(year, month, 0).getDate(),
 				h = [];
 				//console.log(" Year: "+year+ " Month: "+ month + " Total Days: "+days);
@@ -28,7 +27,7 @@ var Calendar = function(){
 
 			};
 			
-			//return h;
+			//return h; //default [0,1,2,3,4,5,6]
 		}
 		
 		//print out the weekdays
@@ -66,34 +65,53 @@ var Calendar = function(){
 			
 			
 			// printing out the dates.
-			for (var i = 0, j=1; i <= h.length; i++) {
-				var date =[],
+			for (var i = 1, j=1, pos = 0; i <= h.length + h[0]; i++, pos++) {
+				var date =[], blankNode = [],
 				    dateColumn = document.createElement('td');
 				    // create text date and add it to the column
-					date[i] = document.createTextNode(i);
-					dateColumn.appendChild(date[i]);
 					
+					date[i] = document.createTextNode(i-h[0]); 
+					blankNode  = document.createTextNode(' ');
+					//dateColumn.appendChild(date[i]);
+					// do insertAt
+					console.log(h);
+					if(pos < h[0])
+					{
+						insertAt(dateColumn, blankNode, pos);
+					}
+					else
+				
+					{
+						insertAt(dateColumn, date[i],  pos)
+				    }
+				
 					// try to get things right
 					if(i%7===0)
 					{
 						// turn to the next row?
 						// how?
-						tab.rows[j].nextElementSibling.appendChild(dateColumn);
+						insertAt(tab.rows[j], dateColumn, pos);
 						j++;
 					}
 					else
 					{
-						console.log(tab.rows[j].cells[2]);
+						insertAt(tab.rows[j], dateColumn, pos)
+						
 						
 					}
+				
+					
 			};
-			
-		
+
 	}
-	
+	//study this.
+	function insertAt(parent, child, n)
+	{
+		parent.insertBefore(child, parent.childNodes[n]);
+	}
 
 }
 
 
 var cal = new Calendar;
-cal.buildMonth(8, 2012);
+cal.buildMonth(9, 2012);
