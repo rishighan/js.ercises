22797d5618113af0e3825c466e23123708336398
Rishi Ghan <rishi.ghan@gmail.com>
2014-10-14 07:14:26 -0400

    adding another code test

---
+++ yodle/scripts.js
@@ -0,0 +1,11 @@
+//scripts.js
+//init like this:
+// var foo = new ratings(ratings-container)
+var ratings =  function(options){
+
+    var args = this.options
+
+  renderStars: function(){
+
+  }
+}
