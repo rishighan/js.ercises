0bb97a874f85b7d6d5db7534ab2a0b855737af73
Rishi Ghan <rishi.ghan@gmail.com>
2014-10-14 16:12:39 -0400

    changes

--- yodle/scripts.js
+++ yodle/scripts.js
@@ -1,13 +1,16 @@
-//scripts.js
-//init like this:
-// var foo = new ratings(ratings-container)
+// scripts.js
+// Rishi Ghan
+// init like this:
+// var foo = new Ratings(opts)
 function Ratings(opts){
 
+    // config
     this.container =  document.getElementById("ratings-container") || this.opts.container;
     this.ratingAttr = "data-rating" || this.opts.ratingAttr,
-    this.totalRatingElements = this.ratingElements.length,
+    // this.totalRatingElements = this.ratingElements.length,
     this.filled_star_graphic = "filled.png" || this.opts.filled_star_graphic,
-    this.half_star_graphic = "half.png" || this.opts.half_star_graphic;
+    this.half_star_graphic = "half.png" || this.opts.half_star_graphic,
+    this.rating_dom_el = "<img/>";
 }
 
 Ratings.prototype = {
@@ -15,16 +18,31 @@ Ratings.prototype = {
    renderStars:function(){
     // retrieve the data-src-rating from the DOM el
     // this will be the child of the rating container
-    // separate decimal portion by doing num%1 and checking against 0.5
 
-    for(var i=0; i < totalRatingElements; i++){
-        console.log(ratingElements[i])
+    // get the elem array
+    var elemArray = this.getRatingElements(this.ratingAttr);
+    //console.log(elemArray)
+
+    for(var i=0, n=elemArray.length; i < n; i++){
+
+        //console.log(elemArray[i].getAttribute(this.ratingAttr));
+        for( var k=0; k < Math.floor(elemArray[i].getAttribute(this.ratingAttr)); k++){
+            //full stars
+            this.rating_dom_el.attr('src') = this.filled_star_graphic;
+            elemArray[i].appendChild(this.rating_dom_el);
+
+        }
+
+        // separate decimal portion by doing num%1 and checking against 0.5
+        if(Number(elemArray[i].getAttribute(this.ratingAttr))%1===0.5){
+            //half star
+        }
     }
 
   },
 
   // get the DOM elements matching the data-rating attribute
-  // the idea is that
+  // return an array of positive matches
   getRatingElements: function(attr){
     var matches =[],
         scope = this.container.getElementsByTagName('*'),
@@ -35,7 +53,13 @@ Ratings.prototype = {
              matches.push(scope[i]);
         }
     }
+    // elements with data-rating attribute
     return matches;
 
   }
 }
+
+
+
+
+
