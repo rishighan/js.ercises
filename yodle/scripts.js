f051a01229b4d5a925f9b8d89fd55831502b329a
Rishi Ghan <rishi.ghan@gmail.com>
2014-10-14 13:18:23 -0400

    added constructor object literal notation

--- yodle/scripts.js
+++ yodle/scripts.js
@@ -1,17 +1,41 @@
 //scripts.js
 //init like this:
 // var foo = new ratings(ratings-container)
-var ratings =  function(options){
+function Ratings(opts){
 
-    var container =  container || options.container,
-        rating_number = rating_number || options.rating_number,
-        filled_star_graphic = "filled.png" || options.filled_star_graphic,
-        half_star_graphic = "half.png" || options.half_star_graphic;
+    this.container =  document.getElementById("ratings-container") || this.opts.container;
+    this.ratingAttr = "data-rating" || this.opts.ratingAttr,
+    this.totalRatingElements = this.ratingElements.length,
+    this.filled_star_graphic = "filled.png" || this.opts.filled_star_graphic,
+    this.half_star_graphic = "half.png" || this.opts.half_star_graphic;
+}
 
-  renderStars: function(){
-    //logic
+Ratings.prototype = {
+
+   renderStars:function(){
     // retrieve the data-src-rating from the DOM el
     // this will be the child of the rating container
-    //separate decimal portion by doing num%1 and checking against 0.5
+    // separate decimal portion by doing num%1 and checking against 0.5
+
+    for(var i=0; i < totalRatingElements; i++){
+        console.log(ratingElements[i])
+    }
+
+  },
+
+  // get the DOM elements matching the data-rating attribute
+  // the idea is that
+  getRatingElements: function(attr){
+    var matches =[],
+        scope = this.container.getElementsByTagName('*'),
+        totalNodes = scope.length;
+
+    for(var i=0; i<totalNodes; i++){
+        if(scope[i].getAttribute(attr)){
+             matches.push(scope[i]);
+        }
+    }
+    return matches;
+
   }
 }
