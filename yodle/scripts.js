a8ddea7ae834b774d6cc4a843476035c986921b0
Rishi Ghan <rishi.ghan@gmail.com>
2014-10-15 08:04:37 -0400

    finalizing functionality and adding styling

--- yodle/scripts.js
+++ yodle/scripts.js
@@ -1,7 +1,9 @@
 // scripts.js
 // Rishi Ghan
+
 // init like this:
 // var foo = new Ratings(opts)
+
 
 // Function to attach our function to the DOM, upon load
 function addEvent(to, type, fn){
@@ -19,44 +21,68 @@ addEvent(window, 'load', function(){
 
 function Ratings(opts){
     // config
-    this.container =  document.body || this.opts.container;
-    this.ratingAttr = "data-rating" || this.opts.ratingAttr,
-    this.filled_star_graphic = "filled.png" || this.opts.filled_star_graphic,
-    this.half_star_graphic = "half.png" || this.opts.half_star_graphic;
+    this.container              =  document.body || this.opts.container;
+    this.ratingAttr             = "data-rating" || this.opts.ratingAttr,
+    this.ratingMax              = 5 || this.opts.ratingMax,
+    this.filled_star_graphic    = "img/filled-star.jpg" || this.opts.filled_star_graphic,
+    this.half_star_graphic      = "img/half.jpg" || this.opts.half_star_graphic,
+    this.empty_star_graphic     = "img/empty.jpg" || this.opts.empty_star_graphic;
 }
 
 Ratings.prototype = {
 
    renderStars:function(){
-    // retrieve the data-rating attribute from the DOM element
-    // get the elem array
+
+    // To test: Adjust the "data-rating" attribute in the HTML to see the results
+
+    // Retrieve the elements with data-rating attribute from the DOM
     var elemArray = this.getElementsByAttr(this.ratingAttr);
-    //console.log(elemArray)
 
+    // Loop over them, checking for integer and decimal
+    // components and drawing solid, half or empty stars accordingly.
     for(var i=0, n=elemArray.length; i < n; i++){
+        var rating = Number(elemArray[i].getAttribute(this.ratingAttr)),
+            ratingDecimal = rating%1,
+            ratingInteger = Math.floor(rating),
+            balance = this.ratingMax-(Math.floor(rating)+1);
 
-        //console.log(elemArray[i].getAttribute(this.ratingAttr));
-        for( var k=0; k < Math.floor(elemArray[i].getAttribute(this.ratingAttr)); k++){
+        for( var k=0; k < ratingInteger; k++){
             //filled stars
             var imgEl = this.createEl('img', this.filled_star_graphic)
             elemArray[i].appendChild(imgEl);
-
         }
 
-        // separate decimal portion by doing num%1 and checking against 0.5
-        if(Number(elemArray[i].getAttribute(this.ratingAttr))%1===0.5){
-            //half star
+        // if rating has a decimal component
+        if(ratingInteger < this.ratingMax && ratingDecimal === 0.5 ){
+
+            // display a half star first
+            var imgEl = this.createEl('img', this.half_star_graphic)
+            elemArray[i].appendChild(imgEl);
+
+            // then loop 5-(rating +1) times and display empty stars
+            // to compensate for the half star
+            for(var j=0; j< balance; j++){
+                var imgEl = this.createEl('img', this.empty_star_graphic)
+                elemArray[i].appendChild(imgEl);
+            }
         }
-        else{
-            //empty star
+        // for integer ratings
+        else if(ratingInteger < this.ratingMax && ratingDecimal === 0 ){
+            // Loop 5- rating times and display empty stars
+            for(var j=0; j< (this.ratingMax - ratingInteger); j++){
+                var imgEl = this.createEl('img', this.empty_star_graphic)
+                elemArray[i].appendChild(imgEl);
+            }
         }
+
     }
 
   },
 
   // Get the DOM elements matching the specified attribute
   // return an array of positive matches
-  // Currently the search area is the body, but can be narrowed down in the config
+  // Currently the search area is the body, but can be narrowed down in the config object
+  // that is passed to the constructor, like so: var opts = {container: "container-el"}
   getElementsByAttr: function(attr){
     var matches =[],
         scope = this.container.getElementsByTagName('*'),
@@ -82,32 +108,43 @@ Ratings.prototype = {
 
   },
 
+  // for the average rating bars
+  // Determines the max number of ratings for a particular star
+  // and then calculates the width of the bar for each star
+  // To test: change the "data-rating-total" attribute in the HTML
   calculateAvgWidth: function(attr){
 
-    var max =0, width =0,
+    var max =0, width =0, values = [],
         bars = this.getElementsByAttr(attr),
         totalBars = bars.length;
 
+    // gather the number of ratings from the bar elements from the DOM
     for(var i =0; i<totalBars; i++){
-        max = Math.max(max, Number(bars[i].getAttribute(attr)));
+        values.push(Number(bars[i].getAttribute(attr)));
+    }
 
-        width = (bars[i].getAttribute(attr) * 150) / max;
+    // find the max number
+    max = this.findMax(values);
 
-        bars[i].style.width = width+"px";
+    // calculate the width
+    for(var j =0; j< totalBars; j++){
+        width = (bars[j].getAttribute(attr) * 150) / max;
+        bars[j].style.width = width+"px";
     }
   },
 
   findMax: function(arr){
-   return Math.max.apply(Math, arr);
+   return Math.max.apply(null, arr);
   }
 
 
 }
 
-
+// init and call methods to render stars and the rating bars.
 var foo = new Ratings(opts);
 foo.renderStars();
-foo.calculateAvgWidth('data-rating-total')
+foo.calculateAvgWidth('data-rating-total');
+
 });
 
 
